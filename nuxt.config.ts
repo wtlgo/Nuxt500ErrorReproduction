dc13fe04c876a3576003e4db4ff99914b5651b62
wtlgo <georgeendo0@gmail.com>
2024-07-04 15:52:56 +0300

    Add a single page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2024-07-04",
   devtools: { enabled: true }
 })
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <span>Index page</span>
+</template>
